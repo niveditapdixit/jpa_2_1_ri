3b51a728e5e072dd1c983db1e7ddcf15c6fe804e
Nivedita_Dixit <niveditapdixit@gmail.com>
2013-01-24 23:30:32 +0530

    committed

---
+++ src/com/adoptjsr/fcms/web/beans/VendorMaintainBean.java
@@ -0,0 +1,86 @@
+/**
+ * 
+ */
+package com.adoptjsr.fcms.web.beans;
+//net.java.adoptjsr.fcms.web. 
+
+import java.io.Serializable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+/**
+ * @author Nivedita_Dixit
+ *
+ */
+public class VendorMaintainBean implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6731561541756443536L;
+	
+	/**
+	 * Logger 
+	 */
+	Logger log = LoggerFactory.getLogger(VendorMaintainBean.class);
+
+	/**
+	 * Default Constructor
+	 */
+	public VendorMaintainBean() {
+		super();
+	}
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String fetch(){
+		
+		return null;
+	}
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String save(){
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String delete(){
+		
+		return null;
+	}	
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String clear(){
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String reset(){
+		
+		return null;
+	}
+	
+
+}
